40da4ef436494fde1f1a55cd0708b66613f6d3e9
春风化雨细如丝 <indiff@126.com>
2026-03-03 11:14:42 +0800

    Delete lexilla directory

--- lexilla/test/examples/perl/class.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use v5.38;
-use feature 'class';
-
-class MyClass::SubClass {
-    method inClass { return 1 }
-    method inClassProto($) { return $_[0] }
-    method inClassAttrib :prototype($) { return $_[0] }
-}
